99b3f56f0f2fa9212338ae3495a5ca3b500123ca
Apurba Saha <47732829+diponsaha007@users.noreply.github.com>
2020-08-13 09:49:26 +0600

    cart price bug fixed hopefully

--- static/js/cart/cart.js
+++ static/js/cart/cart.js
@@ -5,12 +5,27 @@ var subtotal = 0
 var shipping = 60
 var quantity = []
 
-
+function convert_text_to_int(price_text){
+    // console.log("Called")
+    var x =0
+    for(var i=0;i<price_text.length;i++)
+    {
+        // console.log(price_text[i])
+        if(price_text[i]>='0'&&price_text[i]<='9')
+        {
+            var p = price_text[i]-'0'
+            x= x*10 + p
+        }
+    }
+    // console.log(x)
+    return x
+}
 $(document).ready(function () {
     console.log(quantity_input)
     console.log(price_text)
     for (var i = 2; i < quantity_input.length; i++) {
-        prices[i - 2] = parseInt(price_text.eq(i - 2).text().substring(0)) / parseInt(quantity_input.eq(i).val())
+        // console.log("Here")
+        prices[i - 2] = convert_text_to_int(price_text.eq(i - 2).text()) / parseInt(quantity_input.eq(i).val())
         quantity[i - 2] = parseInt(quantity_input.eq(i).val())
         subtotal += prices[i - 2] * quantity[i - 2]
     }
